7a881021a4d19fa5ef3c69fa21a4832fd55a51d8
bossniger <egorbelyakovich2@gmail.com>
2024-12-09 15:13:50 +0300

    csv and pdf export added

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 
-from analytics.views import AnalyticsPageView, AnalyticsView
+from analytics.views import AnalyticsPageView, AnalyticsView, ExportCSVView, ExportPDFView
 
 urlpatterns = [
     path('analytics-page/', AnalyticsPageView.as_view(), name='analytics_page'),
     path('analytics/', AnalyticsView.as_view(), name='analytics_api'),
+    path('export-csv/', ExportCSVView.as_view(), name='export_csv'),
+    path('export-pdf/', ExportPDFView.as_view(), name='export-pdf')
 ]
--- analytics/views.py
+++ analytics/views.py
@@ -1,5 +1,14 @@
+import csv
+from io import BytesIO
+
 from django.db.models import Sum, Case, When, DecimalField
+from django.http import HttpResponse
+from django.utils.timezone import localtime
 from django.views.generic import TemplateView
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
@@ -12,6 +21,7 @@ from budget.models import Transaction
 
 class AnalyticsView(APIView):
     permission_classes = [IsAuthenticated]
+
     def get(self, request):
         if not request.user.is_authenticated:
             raise AuthenticationFailed(detail='Not authenticated')
@@ -28,8 +38,8 @@ class AnalyticsView(APIView):
             end_date = datetime.strptime(end_date, '%Y-%m-%d')
         except ValueError:
             return Response(
-            {"error": "Неверный формат даты. Используйте YYYY-MM-DD."},
-            status=status.HTTP_400_BAD_REQUEST
+                {"error": "Неверный формат даты. Используйте YYYY-MM-DD."},
+                status=status.HTTP_400_BAD_REQUEST
             )
 
         # Фильтрация транзакций по дате
@@ -65,9 +75,83 @@ class AnalyticsView(APIView):
             "period": {"start_date": start_date, "end_date": end_date},
             "total_income": total_income,
             "total_expense": total_expense,
-            "categories": list(analytics) # данные по категориям
+            "categories": list(analytics)  # данные по категориям
         })
 
 
 class AnalyticsPageView(TemplateView):
     template_name = 'reports/user_manual_report.html'
+
+
+class ExportCSVView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        start_date = request.query_params.get('start_date')
+        end_date = request.query_params.get('end_date')
+        transactions = Transaction.objects.filter(
+            user=request.user,
+            date__range=[start_date, end_date]
+        )
+        # Генерация CVV
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename={start_date}-{end_date}.csv'
+        response.write('\ufeff'.encode('utf8')) # поддержка utf-8 в экселе
+        writer = csv.writer(response, delimiter=';')
+        writer.writerow(['Дата', 'Тип', 'Категория', 'Сумма', 'Описание'])
+
+        for transaction in transactions:
+            writer.writerow([
+                localtime(transaction.date).strftime('%Y-%m-%d %H:%M:%S'),
+                transaction.type,
+                transaction.category.name if transaction.category else "Без категории",
+                transaction.amount,
+                transaction.description or "_"
+            ])
+        return response
+
+
+class ExportPDFView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        start_date = request.GET.get('start_date')
+        end_date = request.GET.get('end_date')
+        transactions = Transaction.objects.filter(
+            user=request.user,
+            date__range=[start_date, end_date]
+        ).select_related('category', 'account')
+
+        # Настройка pdf
+        buffer = BytesIO()
+        p = canvas.Canvas(buffer, pagesize=letter)
+
+        # Регистрация шрифта для кириллицы
+        pdfmetrics.registerFont(TTFont("DejaVuSans", "static/fonts/DejaVuSans.ttf"))
+        p.setFont("DejaVuSans", 12)
+
+        # Заголовок
+        p.drawString(100,750, f"Аналитика за период: {start_date} - {end_date}")
+
+        # Отображение транзакций
+        y = 720
+        for transaction in transactions:
+            line = (
+                f"{transaction.date.strftime('%Y-%m-%d %H:%M:%S')} - "
+                f"{transaction.type} - "
+                f"{transaction.category.name if transaction.category else 'Без категории'} - "
+                f"{transaction.description or '-'} - "
+                f"{transaction.amount} - "
+                f"{transaction.account.name if transaction.account else 'Не указан'}"
+            )
+            p.drawString(50, y, line)
+            y -= 20
+            if y < 50: # Создаем новую страницу, если заканчивается место
+                p.showPage()
+                p.setFont("DejaVuSans", 12)
+        p.showPage()
+        p.save()
+        buffer.seek(0)
+        response = HttpResponse(buffer, content_type="application/pdf")
+        response["Content-Disposition"] = f'attachment; filename="analytics_{start_date}_to_{end_date}.pdf"'
+        return response
