1d18a8689063d7fd18b7d77a290188e66563b05d
bossniger <egorbelyakovich2@gmail.com>
2024-12-15 14:33:18 +0300

    Автоматическое обновление курсов валют commit

--- budget/models.py
+++ budget/models.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -67,14 +68,26 @@ class Transaction(models.Model):
         return f'{self.account.name} - {category_name} - {self.amount} {self.currency.code} - {self.date}'
 
     def save(self, *args, **kwargs):
-        if not self.pk: # Если транзакция новая
-            if self.account: # Обновляем баланс только если указан счет
+        if not self.pk:  # Если транзакция новая
+            if self.account:  # Обновляем баланс только если указан счет
+                # Получаем конвертированную сумму, если валюты разные
+                converted_amount = self.converted_amount
+
+                # Обновляем баланс в зависимости от типа транзакции
                 if self.type == 'expense':
-                    self.account.update_balance(-self.amount)
+                    self.account.update_balance(-converted_amount)  # Изменяем на сумму расхода
                 elif self.type == 'income':
-                    self.account.update_balance(self.amount)
+                    self.account.update_balance(converted_amount)  # Изменяем на сумму дохода
+
         super().save(*args, **kwargs)
 
+    @property
+    def converted_amount(self):
+        """ Возвращает сумму, конвертированную в валюту счета. """
+        if self.account.currency != self.currency:
+            conversion_rate = self.currency.rate_to_base / self.account.currency.rate_to_base
+            return self.amount * Decimal(conversion_rate)
+        return self.amount  # Если валюта транзакции совпадает с валютой счета
 
 
 class Tag(models.Model):
